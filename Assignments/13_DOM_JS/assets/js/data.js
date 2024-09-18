7a0811127725538157f22f5fac66df257d462695
Ahmed Yousri <ayousry42@gmail.com>
2024-09-19 01:54:08 +0300

    finished delayed assignment

---
+++ Assignments/13_DOM_JS/assets/js/data.js
@@ -0,0 +1,19 @@
+const movies = [
+    { id: 1, name: "The Matrix", price: 4.99, rating: 4.8 },
+    { id: 2, name: "Inception", price: 5.99, rating: 4.7 },
+    { id: 3, name: "The Dark Knight", price: 6.99, rating: 4.9 },
+    { id: 4, name: "Interstellar", price: 5.49, rating: 4.6 },
+    { id: 5, name: "Pulp Fiction", price: 4.49, rating: 4.5 },
+    { id: 6, name: "The Godfather", price: 6.49, rating: 4.9 },
+    { id: 7, name: "Transformers: Age of Extinction", price: 4.99, rating: 2.9 },
+    { id: 8, name: "The Shawshank Redemption", price: 5.99, rating: 4.9 },
+    { id: 9, name: "Forrest Gump", price: 4.29, rating: 4.7 },
+    { id: 10, name: "Gladiator", price: 4.99, rating: 4.6 },
+    { id: 11, name: "The Avengers", price: 7.99, rating: 4.5 },
+    { id: 12, name: "The Room", price: 2.99, rating: 2.5 },
+    { id: 13, name: "Star Wars: The Phantom Menace", price: 5.29, rating: 3.3 },
+    { id: 14, name: "Batman & Robin", price: 3.99, rating: 2.8 },
+    { id: 15, name: "The Happening", price: 3.49, rating: 2.7 }
+];
+
+export default movies
---
+++ Assignments/13_DOM_JS/assets/js/main.js
@@ -0,0 +1,31 @@
+import movies from "./data.js";
+
+const productsContainer = document.querySelector(".products-container");
+movies.map(movie => {
+    const { name, price, rating,id } = movie;
+    // Create elements for the product
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    const nameElement = document.createElement("h2");
+    nameElement.textContent = name;
+    
+    const formattedPrice = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    });
+
+    const priceElement = document.createElement("p");
+    priceElement.textContent = formattedPrice.format(price);
+    const stars = "⭐".repeat(Math.round(rating));
+
+    const btn = document.createElement("button");
+    btn.textContent = `Buy`;
+    
+    const imageElement = document.createElement("img");
+    imageElement.src = `../assets/imgs/movie_${id}.jpg`;
+
+    const prodArray = [imageElement,nameElement , priceElement , stars , btn]
+
+    productDiv.append(...prodArray)
+    productsContainer.append(productDiv);
+});
